83292cd41a3e96d4787dedaa1caeeb62304c3700
John <83482871+mowgly11@users.noreply.github.com>
2025-11-27 21:19:21 +0100

    feat: added index builder functuin

---
+++ db_operations/indexing.go
@@ -0,0 +1,46 @@
+package db_operations
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func CreateIndex(index *map[string]int) bool {
+	file, err := os.Open("database/database.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+
+	var nextOffset int64 = 0
+
+	for {
+		line, length, err := ReadLineAndLen(r)
+
+		if err == io.EOF && length == 0 {
+			break
+		}
+
+		if line != "" {
+			colon := strings.IndexByte(line, ':')
+			if colon != -1 {
+				key := line[:colon]
+				(*index)[key] = int(nextOffset)
+			}
+		}
+
+		nextOffset += int64(length)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return true
+}
