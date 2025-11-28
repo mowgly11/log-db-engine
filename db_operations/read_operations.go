bb5793772004f6b1bea122e1e13316e38f99dcdb
John <83482871+mowgly11@users.noreply.github.com>
2025-11-28 21:15:15 +0100

    feat: implemented Get function to find a value by key

--- db_operations/read_operations.go
+++ db_operations/read_operations.go
@@ -2,6 +2,7 @@ package db_operations
 
 import (
 	"bufio"
+	//"fmt"
 	"io"
 	"log"
 	"os"
@@ -39,6 +40,34 @@ func OpenFile(path string) *os.File {
 	return file
 }
 
-func Get(key string, index *map[string]int) {
+func Get(key string, index *map[string]int) (string, error) {
+	file := OpenFile("database/database.txt")
+	byteOffset, ok := (*index)[key]
 
+	if !ok {
+		return "", nil
+	}
+
+	_, err := file.Seek(int64(byteOffset), 0)
+	if err != nil {
+		return "", err
+	}
+
+	buffer := make([]byte, 1)
+	result := make([]byte, 0)
+
+	for {
+		_, err := file.Read(buffer)
+		if err != nil {
+			return string(result), err
+		}
+
+		if buffer[0] == '\n' {
+			break
+		}
+
+		result = append(result, buffer[0])
+	}
+
+	return strings.Split(string(result), ":")[1], nil
 }
