308570e2df6e09b76e9dc0f64c8f3f0895d367f0
John <83482871+mowgly11@users.noreply.github.com>
2025-12-04 23:20:08 +0100

    feat!: support for multiple segments and edgecases handling

--- db_operations/write_operations.go
+++ db_operations/write_operations.go
@@ -4,10 +4,37 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/mowgly11/log-db-engine/utils"
 )
 
+const SEGEMENT_SIZE_LIMIT_KB int32 = 1
+
 func Set(key string, value string, index map[string]int) bool {
-	file, err := os.OpenFile("database/database.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	var entryName strings.Builder
+
+	entry, _ := utils.SelectMostRecentSegment()
+
+	if entry == nil {
+		_, name, _ := utils.CreateSegment()
+		entryName.WriteString(name)
+	} else {
+		entryInfo, err := entry.Info()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if (entryInfo.Size() / 1024) >= int64(SEGEMENT_SIZE_LIMIT_KB) {
+			_, name, _ := utils.CreateSegment()
+			entryName.WriteString(name)
+		} else {
+			entryName.WriteString("database/")
+			entryName.WriteString(entry.Name())
+		}
+	}
+
+	file, err := os.OpenFile(entryName.String(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		log.Fatal(err)
@@ -22,12 +49,13 @@ func Set(key string, value string, index map[string]int) bool {
 	}
 
 	var data strings.Builder
-	
+
+	data.WriteString("PUT ")
 	data.WriteString(key)
 	data.WriteRune(':')
 	data.WriteString(value)
 	data.WriteRune('\n')
-	
+
 	if _, err := file.Write([]byte(data.String())); err != nil {
 		log.Println("write error:", err)
 		return false
