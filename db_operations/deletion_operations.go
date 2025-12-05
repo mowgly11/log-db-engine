3efbafe470f93d9347ad271547369faded09d599
John <83482871+mowgly11@users.noreply.github.com>
2025-12-05 22:32:42 +0100

    fix: fixed key deletion to support multiple segments

--- db_operations/deletion_operations.go
+++ db_operations/deletion_operations.go
@@ -6,10 +6,35 @@ import (
 	"strings"
 
 	"github.com/mowgly11/log-db-engine/models"
+	"github.com/mowgly11/log-db-engine/utils"
 )
 
 func Delete(key string, index map[string]models.IndexEntry) bool {
-	file, err := os.OpenFile("database/database.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	var entryName strings.Builder
+
+	entry, _ := utils.SelectMostRecentSegment()
+
+	if entry == nil {
+		_, name, _ := utils.CreateSegment()
+		entryName.WriteString(name)
+	} else {
+		entryInfo, err := entry.Info()
+
+		if err != nil {
+			log.Fatal(err)
+			return false
+		}
+
+		if (entryInfo.Size() / 1024) >= int64(SEGEMENT_SIZE_LIMIT_KB) {
+			_, name, _ := utils.CreateSegment()
+			entryName.WriteString(name)
+		} else {
+			entryName.WriteString("database/")
+			entryName.WriteString(entry.Name())
+		}
+	}
+
+	file, err := os.OpenFile(entryName.String(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		log.Fatal(err)
