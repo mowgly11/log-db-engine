b2c9d451138cf3ae268ffc2c0d68cb95a9c87d73
John <83482871+mowgly11@users.noreply.github.com>
2025-12-07 20:25:14 +0100

    feat: modifier the script to print kv pairs to the std output instead of writing them to a file

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -1,8 +1,7 @@
-file = open("database.txt", "a")
 data = ""
 
-for i in range(10):
-    for j in range(1000 * i, 1000 * i+1):
-        data += f"key{j}:value{j}"
-        file.write(data)
-        data = ""
+
+for i in range(100, 200):
+    data += f"PUT key{i}:value{i}\n"
+
+print(data)
